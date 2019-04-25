b221595f5c38b0baf1c615a4983fa8e7f2199e83
linboyuan <linboyuan@hyholding.cn>
2019-04-25 17:19:12 +0800

    初始化项目

---
+++ app/src/components/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <v-touch v-on:swipeleft="swiperleft"  class="wrapper">
+      <div>1</div>
+    </v-touch>
+    <div>2</div>
+    <div>3</div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    swiperleft() {
+      console.log("1");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
